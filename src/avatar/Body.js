19e9a8c1d8ff281aa05c530dd8f173bba6913634
Angussimons <info@simonepozzobon.com>
2018-11-14 07:02:35 +0100

    Swiper invece del carosello

--- src/avatar/Body.js
+++ src/avatar/Body.js
@@ -9,12 +9,13 @@ import {
 
 import config from '../config'
 import Carousel from 'react-native-snap-carousel'
+import Swiper from 'react-native-swiper'
 
 class Body extends Component {
   constructor() {
     super()
     this.state = {
-      screenWidth: Dimensions.get('window').width * .5
+      screenWidth: Dimensions.get('window').width * .6
     }
   }
 
@@ -38,25 +39,75 @@ class Body extends Component {
     )
   }
 
+
+
   // Render
   render() {
     // Dynamic styles
     const compStyles = StyleSheet.create({})
 
+    const nextButton = <Text style={styles.buttonText}>›</Text>
+    const prevButton = <Text style={styles.buttonText}>‹</Text>
+
     // Component
+    // return (
+    //   <View style={{ flex: 1, justifyContent: 'center',  alignItems: 'center' }}>
+    //     <Carousel
+    //       data={config.avatar.body}
+    //       renderItem={this.renderItem}
+    //       sliderWidth={this.state.screenWidth}
+    //       itemWidth={this.state.screenWidth}
+    //     />
+    //   </View>
+    // );
     return (
-      <View style={{ flex: 1, justifyContent: 'center',  alignItems: 'center' }}>
-        <Carousel
-          data={config.avatar.body}
-          renderItem={this.renderItem}
-          sliderWidth={this.state.screenWidth}
-          itemWidth={this.state.screenWidth}
-        />
+      <View style={{ marginHorizontal: 40 }}>
+        <Swiper
+          showsButtons={true}
+          showsPagination={false}
+          automaticallyAdjustContentInsets={true}
+          width={this.state.screenWidth}
+          height={this.state.screenWidth}
+          nextButton={nextButton}
+          prevButton={prevButton}
+          buttonWrapperStyle={styles.buttonWrapperStyle}
+        >
+          {config.avatar.body.map((item, index) => {
+            return (
+                <Image
+                  style={{
+                    width: this.state.screenWidth,
+                    height: this.state.screenWidth,
+                    resizeMode: 'contain'
+                  }}
+                  source={item.img}
+                />
+            )
+          })}
+        </Swiper>
       </View>
-    );
+    )
   }
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  buttonText: {
+    color: config.colors.black,
+    fontSize: 40,
+  },
+
+  buttonWrapperStyle: {
+    backgroundColor: 'transparent',
+    flexDirection: 'row',
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    flex: 1,
+    paddingHorizontal: 1,
+    paddingVertical: 10,
+    justifyContent: 'space-between',
+    alignItems: 'center'
+  },
+})
 
 export default Body;
--- src/avatar/Feet.js
+++ src/avatar/Feet.js
@@ -9,6 +9,7 @@ import {
 
 import config from '../config'
 import Carousel from 'react-native-snap-carousel'
+import Swiper from 'react-native-swiper'
 
 class Feet extends Component {
   constructor() {
@@ -43,20 +44,68 @@ class Feet extends Component {
     // Dynamic styles
     const compStyles = StyleSheet.create({})
 
+    const nextButton = <Text style={styles.buttonText}>›</Text>
+    const prevButton = <Text style={styles.buttonText}>‹</Text>
+
     // Component
+    // return (
+    //   <View style={{ flex: 1, justifyContent: 'center',  alignItems: 'center' }}>
+    //     <Carousel
+    //       data={config.avatar.leg}
+    //       renderItem={this.renderItem}
+    //       sliderWidth={this.state.screenWidth}
+    //       itemWidth={this.state.screenWidth}
+    //     />
+    //   </View>
+    // );
     return (
-      <View style={{ flex: 1, justifyContent: 'center',  alignItems: 'center' }}>
-        <Carousel
-          data={config.avatar.leg}
-          renderItem={this.renderItem}
-          sliderWidth={this.state.screenWidth}
-          itemWidth={this.state.screenWidth}
-        />
+      <View style={{ marginHorizontal: 40 }}>
+        <Swiper
+          showsButtons={true}
+          showsPagination={false}
+          automaticallyAdjustContentInsets={true}
+          width={this.state.screenWidth}
+          height={this.state.screenWidth}
+          nextButton={nextButton}
+          prevButton={prevButton}
+          buttonWrapperStyle={styles.buttonWrapperStyle}
+        >
+          {config.avatar.leg.map((item, index) => {
+            return (
+                <Image
+                  style={{
+                    width: this.state.screenWidth,
+                    height: this.state.screenWidth,
+                    resizeMode: 'contain'
+                  }}
+                  source={item.img}
+                />
+            )
+          })}
+        </Swiper>
       </View>
-    );
+    )
   }
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  buttonText: {
+    color: config.colors.black,
+    fontSize: 40,
+  },
+
+  buttonWrapperStyle: {
+    backgroundColor: 'transparent',
+    flexDirection: 'row',
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    flex: 1,
+    paddingHorizontal: 1,
+    paddingVertical: 10,
+    justifyContent: 'space-between',
+    alignItems: 'center'
+  },
+})
 
 export default Feet;
