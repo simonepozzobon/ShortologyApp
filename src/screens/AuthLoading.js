cfa3e28c702a1fdf1bd3cd9849cbd7076bf015ee
Angussimons <info@simonepozzobon.com>
2018-11-15 05:56:12 +0100

    Routing per l'autorizzazione completato

--- src/screens/AuthLoading.js
+++ src/screens/AuthLoading.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import {
   ActivityIndicator,
+  AsyncStorage,
   Dimensions,
   Image,
   StyleSheet,
@@ -9,6 +10,7 @@ import {
 } from 'react-native'
 
 import { MainTemplate } from '../presentation'
+import axios from 'axios'
 import config from '../config'
 
 class AuthLoading extends Component {
@@ -20,9 +22,78 @@ class AuthLoading extends Component {
     this._bootstrapAsync()
   }
 
-  _bootstrapAsync = async () => {
-    const test = 'test'
+  _redirectAuthorized = () => {
+    this.props.navigation.navigate('App')
   }
+
+  _redirectUnauthorized = () => {
+    this.props.navigation.navigate('Auth')
+  }
+
+  _bootstrapAsync = async () => {
+    AsyncStorage.getItem('token').then((token) => {
+      if (token) {
+        // c'è un token e va validato
+        this._validateToken(token)
+      }  else {
+        // user non è loggato
+        this._redirectUnauthorized()
+      }
+    }).done()
+  }
+
+  _validateToken = (token) => {
+    axios.defaults.headers.common['Authorization'] = 'Bearer ' + token
+    axios.get(config.api.path + '/user').then(response => {
+
+      // formatta la risposta e la salva nel dispositivo
+      const user = JSON.stringify(response.data)
+      AsyncStorage.setItem('user', user)
+      this._redirectAuthorized()
+
+    }).catch(err => {
+      // se non riesce a recuperare l'utente prova ad autenticarlo di nuovo
+      this._reAuthenticateUser()
+    })
+  }
+
+  _reAuthenticateUser = () => {
+    const email = AsyncStorage.getItem('email')
+    const password = AsyncStorage.getItem('password')
+
+    let data = new FormData()
+    data.append('email', email)
+    data.append('password', password)
+
+    axios.post(config.api.path + '/login', data).then(response => {
+      // user logged in
+      if (response.data.success) {
+
+        // Salva il token
+        AsyncStorage.setItem('token', response.data.token)
+
+        // Salva l'utente
+        const user = JSON.stringify(response.data.user)
+        AsyncStorage.setItem('user', user)
+
+        // Salva email
+        AsyncStorage.setItem('email', this.state.email)
+
+        // Salva password
+        AsyncStorage.setItem('password', this.state.password)
+        this._redirectAuthorized()
+
+      } else {
+        // se il login non funziona l'utente dovrà loggarsi di nuovo manualmente
+        this.removeItemValue('token')
+        this.removeItemValue('user')
+        this.removeItemValue('email')
+        this.removeItemValue('password')
+        this._redirectUnauthorized()
+      }
+    })
+  }
+
   // Render
   render() {
     // Dynamic styles
@@ -33,7 +104,6 @@ class AuthLoading extends Component {
         alignItems: 'center',
         width: this.state.screenWidth * 0.5,
         height: this.state.screenWidth * 0.5,
-        // backgroundColor: 'blue',
       },
 
       companyName: {
@@ -43,7 +113,6 @@ class AuthLoading extends Component {
         fontSize: 20,
         fontWeight: 'bold',
         color: config.colors.black,
-        // backgroundColor: 'green',
       }
     })
 
@@ -87,7 +156,6 @@ class AuthLoading extends Component {
 
 const styles = StyleSheet.create({
   container: {
-    // backgroundColor: 'white',
     flex: 12
   },
 
@@ -95,12 +163,10 @@ const styles = StyleSheet.create({
     flex: 5,
     justifyContent: 'flex-end',
     alignItems: 'center',
-    // backgroundColor: 'purple',
   },
 
   headerblock: {
     alignItems: 'center',
-    // backgroundColor: 'white',
   },
 
   loaderContainer: {
