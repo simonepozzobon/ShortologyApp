a93ff5f54dc57a1ced10b94f3693dcaca1c17bf1
Angus Simons <info@simonepozzobon.com>
2021-12-13 11:56:55 +0100

    Build shortologyAPp

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 /** @format */
 
+
+// import applyDecoratedDescriptor from '@babel/runtime/helpers/esm/applyDecoratedDescriptor'
+// import initializerDefineProperty from '@babel/runtime/helpers/esm/initializerDefineProperty'
+//
+// Object.assign(babelHelpers, { applyDecoratedDescriptor, initializerDefineProperty })
+
 import {AppRegistry} from 'react-native';
+
 import App from './App';
+// const App = require('./App').default
 import {name as appName} from './app.json';
 
 AppRegistry.registerComponent(appName, () => App);
